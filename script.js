1b63d4abda116e2d99506948c058940e2056e973
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-10-09 14:59:36 -0400

    Working on time past future and present

--- script.js
+++ script.js
@@ -12,14 +12,18 @@ $("#currentDay").text(currentDay.format("LLLL"));
 // User interaction
 // when user look at the time block the colors show current, past and future time.
 // function timeBlockHour(){
-//     $(".form-control").each(function () {
+    $(".form-control").each(function () {
+        hourTracker = parseInt(hourTracker)
         
-//     }
-// }
+        if (hourTracker >)
+    }
+// };
 // when user click the button on the right it will save to local storage.
 $(".saveBtn").on("click", function(){
     var userInput = $(this).siblings(".form-control").val();
+    console.log(userInput)
     var hours = $(this).siblings(".input-group-prepend").text();
+    console.log(hours)
     localStorage.setItem(hours, JSON.stringify(userInput));
 });
 
