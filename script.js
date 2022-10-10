278df051a6b1d456750189019a90bce2c35d564f
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-10-09 20:17:11 -0400

    localstorage now persist when I click refresh

--- script.js
+++ script.js
@@ -27,11 +27,21 @@ function timeBlockHour() {
     });
 }
 // when user click the button on the right it will save to local storage.
-$("#9").val(JSON.parse(localStorage.getItem(9)));
+$("#9").val(JSON.parse(localStorage.getItem("9AM")));
+$("#10").val(JSON.parse(localStorage.getItem("10AM")));
+$("#11").val(JSON.parse(localStorage.getItem("11AM")));
+$("#12").val(JSON.parse(localStorage.getItem("12AM")));
+$("#13").val(JSON.parse(localStorage.getItem("1PM")));
+$("#14").val(JSON.parse(localStorage.getItem("2PM")));
+$("#15").val(JSON.parse(localStorage.getItem("3PM")));
+$("#16").val(JSON.parse(localStorage.getItem("4PM")));
+$("#17").val(JSON.parse(localStorage.getItem("5PM")));
+
+
 $(".saveBtn").on("click", function(){
-    var userInput = $(this).siblings(".form-control").val();
+    var userInput = $(this).siblings(".form-control").val().trim();
     console.log(userInput)
-    var hours = $(this).siblings(".input-group-prepend").text();
+    var hours = $(this).siblings(".input-group-prepend").text().trim();
     console.log(hours)
     localStorage.setItem(hours, JSON.stringify(userInput));
 });
